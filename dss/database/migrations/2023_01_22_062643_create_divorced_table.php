ecdab80f72b75bcaf081a6dbf4c2ac00823d873f
shahriar-171272 <spranto22@gmail.com>
2023-01-22 12:29:40 +0600

    Create Divorced Table

---
+++ dss/database/migrations/2023_01_22_062643_create_divorced_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('divorced', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('age');
+            $table->date('enrolled_date');
+            $table->string('educational_qualification');
+            $table->string('occupation');
+            $table->string('interest/skill/special_skill');
+            $table->integer('monthly_income');
+            $table->string('income_source');
+            $table->integer('job_experience');
+            $table->string('type_of_wealth');
+            $table->string('depended_on');
+            $table->string('depends_on_her');
+            $table->string('working_ability');
+            $table->date('date_of_being_divorced');
+            $table->integer('number_of_remarriage');
+            $table->string('health_status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('divorced');
+    }
+};
