cb05eb772f237e8db8c4203f106366f67e89d2e1
shahriar-171272 <spranto22@gmail.com>
2023-01-22 13:13:20 +0600

    Create NGO Type Table

---
+++ dss/database/migrations/2023_01_22_070721_create_ngo_type_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ngo_type', function (Blueprint $table) {
+            $table->id();
+            $table->string('ngo_name');
+            $table->string('total_listed_member');
+            $table->string('type_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ngo_type');
+    }
+};
