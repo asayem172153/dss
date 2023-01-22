4c5a67cccffbc9e74613e1b30cdd66ac10a4bcc2
shahriar-171272 <spranto22@gmail.com>
2023-01-22 14:31:03 +0600

    Create Training Table

---
+++ dss/database/migrations/2023_01_22_081550_create_training_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('training', function (Blueprint $table) {
+            $table->id();
+            $table->string('training_name');
+            $table->date('training_date');
+            $table->integer('no_of_trainee_perticipated');
+            $table->string('training_type');
+            $table->string('address');
+            $table->integer('upazila_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('training');
+    }
+};
