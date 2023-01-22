902b76e60c7a5879d7a9cd43402e17ce00309589
shahriar-171272 <spranto22@gmail.com>
2023-01-22 13:02:36 +0600

    Create Product From NGO Table

---
+++ dss/database/migrations/2023_01_22_065432_create_product_from_ngo_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_from_ngo', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_name');
+            $table->string('ngo_name');
+            $table->string('market_monitoring');
+            $table->string('available_in_local_market?');
+            $table->string('available_in_foreign_market?');
+            $table->integer('market_price_local');
+            $table->integer('market_price_foreign');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_from_ngo');
+    }
+};
