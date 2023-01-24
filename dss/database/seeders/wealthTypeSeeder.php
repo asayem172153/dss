a68a5b8a3912c5149a18804eb060285dddcb5a78
shahriar-171272 <spranto22@gmail.com>
2023-01-24 12:42:30 +0600

    Seeding Wealth Type Table

---
+++ dss/database/seeders/wealthTypeSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Faker\Factory as Faker;
+
+class wealthTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        foreach (range(1,10) as $value){
+            DB::table('wealth_type')->insert([
+                'id'=> $value,
+                'wealth_type_name'=>$faker->randomElement(['Type-1', 'Type-2', 'Type-3', 'Type-4', 'Type-5',
+                                                        'Type-6', 'Type-7', 'Type-8', 'Type-9', 'Type-10'])
+            ]);
+        }
+    }
+}
